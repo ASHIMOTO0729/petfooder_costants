ee380a7a702b4d489418dfac38de80d160236491
ASHIMOTO <mukaide.3200@gmail.com>
2025-07-05 22:02:58 +0900

    Update firebase_realtime.dart
    Co-Authored-By: Hpiger <129212015+Hpiger@users.noreply.github.com>

--- lib/firebase_realtime.dart
+++ lib/firebase_realtime.dart
@@ -3,20 +3,25 @@ class FRDnaming {
   static const String baseCountPath = 'count';
   static const String baseUserPath = 'user';
 
+  //
+
   //--------------------------------
   // 投稿カウント
   //--------------------------------
+  static const String subPostCount = 'post_count';
   static const String pathPostViewCounts = 'post_view_counts'; //視聴数
   static const String pathPostLikeCounts = 'post_like_counts'; //いいね数
   static const String pathPostShareCounts = 'post_share_counts'; //シェア数
   static const String pathPostCommentCounts = 'post_comment_counts'; //コメント数
   //----
+  
   static const String pathPostCommentReplyCounts = 'post_comment_reply_counts'; //コメント返信数
   static const String pathPostCommentLikeCounts = 'post_comment_like_counts'; //コメントいいね数
 
   //--------------------------------
   // ペットカウント
   //--------------------------------
+  static const String subPetCount = 'pet_count';
   static const String pathPetViewCounts = 'pet_post_view_counts'; //合計視聴数
   static const String pathPetLikeCounts = 'pet_post_like_counts'; //合計投稿いいね数
   static const String pathPetReviewLikeCounts = 'pet_review_like_counts'; //合計レビューいいね数
@@ -24,14 +29,19 @@ class FRDnaming {
   //--------------------------------
   // ユーザーカウント
   //--------------------------------
+
   static const String pathAccountBan = 'account_ban'; //アカウントバン数
   //----
   //カウント
+  static const String subUserCount = 'user_count';
+
   static const String pathUserPostCounts = 'user_post_counts'; //投稿数
   static const String pathUserReviewCounts = 'user_review_counts'; //レビュー数
   static const String pathUserTicketCounts = 'user_ticket_counts'; //チケット枚数
   //----
   //通知の許可
+  static const String subUserNotification = 'user_notification';
+
   static const String pathUserNotificationTopicLikeReview = 'user_notification_like_review';
   static const String pathUserNotificationTopicLikePost = 'user_notification_like_post';
   static const String pathUserNotificationTopicLikePostComment = 'user_notification_like_post_comment';
@@ -40,6 +50,8 @@ class FRDnaming {
   static const String pathUserNotificationTopicAppDebug = 'user_notification_app_debug';
   //----
   //チケット
+  static const String subUserTicketLog = 'user_ticket_log';
+
   static const String pathUserTicketLogGetReviewAd = 'user_ticket_log_get_review_ad'; //レビュー広告チケット取得数
   static const String pathUserTicketLogGetReviewNoAd = 'user_ticket_log_get_review_no_ad'; //レビュー非広告チケット取得数
   static const String pathUserTicketLogLostPost = 'user_ticket_log_lost_post'; //投稿消費チケット数
